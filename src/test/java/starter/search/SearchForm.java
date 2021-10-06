59c807e551e0f4fe248c58a622f76303db792e97
ricardo larrahondo <ricardolarrahondo2@hotmail.com>
2021-10-06 13:02:46 -0500

    Initialize screenplay

---
+++ src/test/java/starter/search/SearchForm.java
@@ -0,0 +1,8 @@
+package starter.search;
+
+import org.openqa.selenium.By;
+
+class SearchForm {
+    static By SEARCH_FIELD = By.name("q");
+    static By SEARCH_BUTTON = By.cssSelector(".search__button");
+}
