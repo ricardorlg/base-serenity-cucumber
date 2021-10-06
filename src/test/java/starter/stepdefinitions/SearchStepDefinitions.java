aa6cb3cfb26fb986b6e272d6703df7aa8b9360f5
ricardo larrahondo <ricardolarrahondo2@hotmail.com>
2021-10-06 13:39:16 -0500

    :fire: Update tests moving screenplay classes to main package
    :art: Publish reports to github pages

--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -6,12 +6,19 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.questions.targets.TheTarget;
+import org.hamcrest.CoreMatchers;
 import starter.navigation.NavigateTo;
 import starter.search.LookForInformation;
 import starter.search.WikipediaArticle;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.questions.targets.TheTarget.textOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class SearchStepDefinitions {
 
@@ -29,8 +36,12 @@ public class SearchStepDefinitions {
 
     @Then("{actor} should see information about {string}")
     public void should_see_information_about(Actor actor, String term) {
-        actor.attemptsTo(
-                Ensure.that(WikipediaArticle.HEADING).hasText(term)
+        actor.should(
+                seeThat(
+                        textOf(WikipediaArticle.HEADING),
+                        equalTo(term)
+                )
         );
+
     }
 }
--- src/test/resources/features/search/search_by_keyword.feature
+++ src/test/resources/features/search/search_by_keyword.feature
@@ -1,7 +1,9 @@
 Feature: Search by keyword
 
-  Scenario: Searching for a term
-    Given Sergey is researching things on the internet
-    When he looks up "Cucumber"
-    Then he should see information about "Cucumber"
+  Rule: User can search using any term
+
+    Example: Searching for a term
+      Given Sergey is researching things on the internet
+      When he looks up "Cucumber"
+      Then he should see information about "Cucumber"
 
